effc7cc3b8343b45449485f73465c3422330ac52
Kengo Nakatsuka <kengo.nakatsuka@gmail.com>
2018-02-24 23:55:16 +0900

    Add conditionalPutRetentionPolicy

--- test/test.js
+++ test/test.js
@@ -1,14 +1,26 @@
 const test = require('tape');
 const {
+  conditionalPutRetentionPolicy,
   removeEmptyLogGroups,
 } = require('..');
 const AWS = require('aws-sdk');
 
 AWS.config.update({ region: process.env.AWS_REGION || 'us-west-2' });
 
-test('test removeEmptyLogGroups', t => {
+test('test conditionalPutRetentionPolicy', t => {
   t.plan(2);
 
+  conditionalPutRetentionPolicy({
+    retentionInDays: 7,
+  }, (err, result) => {
+    t.error(err);
+
+    t.ok(Array.isArray(result.logGroups));
+  });
+});
+
+test('test removeEmptyLogGroups', t => {
+  t.plan(2);
   removeEmptyLogGroups({}, (err, result) => {
     t.error(err);
 
